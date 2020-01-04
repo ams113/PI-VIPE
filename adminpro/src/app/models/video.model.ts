44f95dda3456f20bde7b91b8524b7b2b36e8750e
tonimtz <yotoni7@gmail.com>
2020-01-04 20:33:04 +0100

    cargar lista de videos en el dashboard desde el back

--- adminpro/src/app/models/video.model.ts
+++ adminpro/src/app/models/video.model.ts
@@ -3,6 +3,7 @@ export class Video {
     constructor (
         public nombre: string,
         public tipo: string,
+        public descripcion: string,
         public img?: string,
         public _id?: string
     ) { }
--- adminpro/src/app/pages/dashboard/dashboard.component.ts
+++ adminpro/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from '../../models/video.model';
+import { VideoService } from '../../services/video/video.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  videos: Video[] = [];
+  totalRegistros = 0;
+  desde = 0;
+  cargando = true;
+
+  constructor( public _videoService: VideoService) { }
 
   ngOnInit() {
+    this.cargarVideos();
+  }
+
+  cargarVideos() {
+    this.cargando = true;
+    this._videoService.cargarVideos( this.desde )
+    .subscribe( (resp: any) => {
+        this.videos = resp.videos;
+        console.log(this.videos);
+        this.totalRegistros = resp.total;
+      this.cargando = false;
+    });
   }
 
 }
